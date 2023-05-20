4f0de154c829becd3233a3721ac52b2c4242f414
ignxcy <ignacyy@proton.me>
2023-05-20 18:47:42 +0200

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const submitButton = document.getElementById("submit") as HTMLButtonElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+submitButton.onclick = () => submit();
+
+function submit() {
+  const elementInput = document.getElementById("element") as HTMLInputElement;
+  const element: string = elementInput.value;
+  const colorInput = document.getElementById("color") as HTMLInputElement;
+  const color: string = colorInput.value;
+  const valueInput = document.getElementById("value") as HTMLInputElement;
+  const value: string = valueInput.value;
+  const output = document.getElementById("output");
+
+  if (element !== "") {
+    if (output) {
+      output.innerHTML = `<${element} style="color: ${color};">${value}</${element}>`;
+    }
+  }
+  else {
+    if (output) {
+      output.innerHTML = `<span style="color: ${color};">${value}</span>`;
+    }
+  }
+}
